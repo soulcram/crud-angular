24d2a9023a2c86caade847c2c324cf2781163f23
Soulcram <mpwmnw@gmail.com>
2024-10-20 13:03:56 -0300

    Atualizando angular

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, delay, tap } from 'rxjs/operators';
+
+import { Course } from '../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = 'api/courses';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list(){
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      delay(1000),
+      tap(course => console.log(course))
+    );
+  }
+}
---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+@NgModule({
+  exports: [MatCardModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+
+})
+export class AppMaterialModule { }
